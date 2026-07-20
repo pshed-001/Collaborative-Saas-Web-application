f55ab5a53b9700b807a00bec503286d0c54a5014
pshed-001 <philipshedrack001@gmail.com>
2026-07-20 13:44:34 +0300

    updated to postgres and uodated schema

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,9 +1,25 @@
-import "dotenv/config";
+/*import "dotenv/config";
 import { PrismaClient } from '../../generated/prisma/client.ts';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-
+// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+/*
 const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaBetterSqlite3({ url : connectionString })
+const prisma = new PrismaClient({ adapter });
+
+const prisma = new PrismaClient()
+export default prisma; */
+
+import "dotenv/config";
+import { PrismaClient } from '../../generated/prisma/client.ts'; 
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from "pg"
+// Initialize the native PG pool
+const pool = new pg.Pool({ 
+  connectionString: process.env.DATABASE_URL 
+});
+
+// Wrap it with the Prisma 7 PostgreSQL adapter
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
