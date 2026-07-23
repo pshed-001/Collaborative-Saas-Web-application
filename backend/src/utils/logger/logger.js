d8ce35cab0ef65fd2da7f2eb7cd22ece33e4ea04
pshed-001 <philipshedrack001@gmail.com>
2026-07-23 21:29:52 +0300

    updated logger

--- backend/src/utils/logger/logger.js
+++ backend/src/utils/logger/logger.js
@@ -1,6 +1,6 @@
 import { customLogger } from "./customLogger.js";
 
 let logger = null
-if(process.env.NODE_ENV!== "production")
+if(process.env.NODE_ENV === "production")
     logger = customLogger()
 export default logger
