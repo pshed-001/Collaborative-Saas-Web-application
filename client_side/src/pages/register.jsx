eaeb32c27184fd52c1142780bd7da008fd06d988
Pshed-001 <philipshedrack001@gmail.com>
2026-08-17 23:33:03 +0200

    Update register.jsx

--- client_side/src/pages/register.jsx
+++ client_side/src/pages/register.jsx
@@ -29,7 +29,7 @@ const schema = z
     .regex(/[0-9]/, 'Password must contain at least one number')
     .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character'),
 }),
-confirmPassword: z
+    confirmPassword: z
     .string()
     .min(10, 'Password must be at least 10 characters')
     .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
