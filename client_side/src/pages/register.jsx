d004d730aafa4829326c565b1fe395beeb378247
Pshed-001 <philipshedrack001@gmail.com>
2026-08-17 23:37:49 +0200

    Update register.jsx

--- client_side/src/pages/register.jsx
+++ client_side/src/pages/register.jsx
@@ -28,15 +28,13 @@ const schema = z
     .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
     .regex(/[0-9]/, 'Password must contain at least one number')
     .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character'),
-}),
     confirmPassword: z
     .string()
     .min(10, 'Password must be at least 10 characters')
     .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
     .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
     .regex(/[0-9]/, 'Password must contain at least one number')
-    .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character'),
-})
+    .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character')
   .refine((d) => d.password === d.confirmPassword, {
     message: 'Passwords do not match',
     path: ['confirmPassword'],
